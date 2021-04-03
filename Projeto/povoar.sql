769c3d77bafd052b3e534e111f549ac05928ddbc
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-04-03 22:07:24 +0100

    Fixes povoar.sql. Was really just a mess but now it's clean and works really well!

--- Projeto/povoar.sql
+++ Projeto/povoar.sql
@@ -1,39 +1,53 @@
 PRAGMA FOREIGN_KEYS = ON;
 
-
-
 INSERT INTO cliente(nif, nome, morada, contacto)
 VALUES 
-	(233143335, "Afonso Carvalho", "Rua Vasco de Lobeira, 255", 936653870),
-	(223459225, "Sebastião Silva", "Rua do Bonfim, 34", 930675100),
-	(133657483, "Maria Sottomayor", "Avenida Fernão Magalhães, 1504", 911183472),
-	(248564348, "Francisca Pinto Cruz", "Rua Eça de Queirós, 544", 919942254);
+	(239143555, "Afonso Carvalho",      "Rua Vasco de Lobeira, 255",      936653870),
+	(223459225, "Sebastião Silva",      "Rua do Bonfim, 34",              930675100),
+	(133657483, "Maria Sottomayor",     "Avenida Fernão Magalhães, 1504", 911183472),
+	(248564348, "Francisca Pinto Cruz", "Rua Eça de Queirós, 544",        919942254);
 
 
 INSERT INTO projeto(nr_projeto, orcamento, prazo)
 VALUES
-	(1, 150000, 2021-11-01),
-	(2, 870000, 2022-04-29),
-	(3, 1450000, 2022-01-15),
-	(4, 730000, 2021-12-10);
-
-
-INSERT INTO obra(nr_obra, data_inicio, custo, area_terreno, area_interior, estado, rua, nr_porta, cod_postal)
-VALUES
-	(1, 2020-03-01, 110000, 230, 190, "Por Aprovar", "Avenida da Boavista", 925, 4050112),
-	(2, 2020-11-01, 60000, 110, 70, "Em Andamento", "Rua das Flores", 21, 4050262),
-	(3, 2021-11-01, 430000, 315.5, 195.5, "Aprovada", "Rua das Flores", 195, 4050262),
-	(4, 2022-04-29, 570000, 520, 420, "Em Andamento", "Praça da República", 23, 4050495);
+	(1, 150000,  730),
+	(2, 870000,  365),
+	(3, 1450000, 100),
+	(4, 730000,  250);
 
 
 INSERT INTO estado(designacao)
 VALUES
 	("Por aprovar"),
 	("Aprovada"),
-	("Em Andamento"),
+	("Em andamento"),
 	("Parada"),
 	("Interrompida"),
 	("Concluída");
+
+
+INSERT INTO localizacao(rua, nr_porta, cod_postal, cidade)
+VALUES
+	("Avenida Dr. António Machado", 130, "4100-068", "Leça"),
+	("Avenida da Boavista",	925, "4050-112", "Porto"),
+	("Rua das Flores", 195, "4050-262", "Porto"),
+	("Rua das Flores", 21, "4050-262", "Porto"),
+	("Rua Luís de Camões", 44, "4785-331", "Trofa"),
+	("Praça da República",23, "4050-495", "Porto");
+
+
+INSERT INTO obra(nr_obra, data_inicio, custo, area_terreno, area_interior, estado, rua, nr_porta, cod_postal)
+VALUES
+	(1, "2020-03-01", 110000, 230,   190, "Por aprovar",  "Avenida da Boavista", 925, "4050-112"),
+	(2, "2020-11-01", 60000,  110,   70, "Em andamento", "Rua das Flores", 	 21,  "4050-262"),
+	(3, "2021-11-01", 430000, 315.5, 195.5, "Aprovada",     "Rua das Flores",      195, "4050-262"),
+	(4, "2022-04-29", 570000, 520,   420,   "Em andamento", "Praça da República",  23,  "4050-495");
+
+
+INSERT INTO estaleiro(nr_estaleiro, capacidade, rua, nr_porta, cod_postal)
+VALUES
+	(1, 900, "Avenida Dr. António Machado", 130, "4100-068"),
+	(2, 1700, "Rua Luís de Camões", 44, "4785-331");
 
 
 INSERT INTO material(codigo, designacao, quantidade_disp, custo, guardado)
@@ -44,34 +58,19 @@ VALUES
 	(109, "Tijolo", 4900, 130000, 1);
 
 
-INSERT INTO localizacao(rua, nr_porta, cod_postal, cidade)
-VALUES
-	("Avenida Dr. António Machado", 130, 4100068, "Leça"),
-	("Rua Luís de Camões", 44, 4785331, "Trofa"),
-	("Avenida da Boavista", 925, 4050112, "Porto"),
-	("Rua das Flores", 195, 4050262, "Porto"),
-	("Praça da República", 23, 4050495, "Porto");
-
-
-INSERT INTO estaleiro(nr_estaleiro, capacidade, rua, nr_porta, cod_postal)
-VALUES
-	(1, 900, "Avenida Dr. António Machado", 130, 4100068),
-	(2, 1700, "Rua Luís de Camões", 44, 4785331);
-
-
-INSERT INTO veiculo(matricula, consumo_medio, despesa, tipo, estaleiro)
-VALUES
-	("71-BC-10", 8.5, 25000, "Comercial", 1),
-	("15-VV-08", 12, 74000, "Comercial", 1),
-	("02-LP-41", 21.4, 79000, "Camião", 2),
-	("11-ZX-50", 34.1, 160000, "Retroescavadora", 3);
-
-
 INSERT INTO tipo_de_veiculo(designacao, categoria)
 VALUES
 	("Comercial", "B"),
 	("Camião", "C"),
 	("Retroescavadora", "CE");
+
+
+INSERT INTO veiculo(matricula, consumo_medio, despesa, tipo, estaleiro)
+VALUES
+	("71-BC-10", 8.5,  25000, "Comercial", 1),
+	("15-VV-08", 12,   74000, "Comercial", 1),
+	("02-LP-41", 21.4, 79000, "Camião", 2),
+	("11-ZX-50", 34.1, 160000, "Retroescavadora", 2);
 
 
 INSERT INTO especialidade(designacao)
@@ -94,24 +93,24 @@ VALUES
 	(293373391, "Isabel Cruz",      943590748, 1300, "Mestrado",          29, "F"),
 	(214572391, "Mário Vargas",     912935874, 1500, "Ensino Secundário", 42, "M"),
 	(243095478, "Fábio Fernandes",  930002549, 900,  "Ensino Secundário", 50, "M"),
-	(290345724, "Mauro Carvalhal",  917723422, 600,  "Ensino Secundário", 30, "M"),
+	(290345724, "Mauro Carvalhal",  917723422, 670,  "Ensino Secundário", 30, "M"),
 	(134590229, "José Silva",       968823421, 1600, "Ensino Básico",     24, "O"),
 	(183978234, "Tiago Alves",      968823010, 700,  "Ensino Básico",     29, "M"),
 	(119355101, "João Alves",       911199200, 1100, "Ensino Secundário", 28, "M"),
 	(210098723, "Marta Almeida",    961193209, 1100, "Ensino Secundário", 34, "F");
 
 
-INSERT INTO empregado_de_escritório(nif, estacao_trabalho, especialidade)
+INSERT INTO empregado_de_escritorio(nif, estacao_trabalho, especialidade)
 VALUES
-	(287879833, "Desenho", "Arquiteto"),
-	(220209493, "Finanças", "Gestor"),
-	(285852988, "Desenho", "Engenheiro Civil"),
-	(210103903, "Finanças", "Contabilista");
+	(211003392, "Desenho", "Arquiteto"),
+	(223573395, "Finanças", "Gestor"),
+	(293373391, "Desenho", "Engenheiro Civil"),
+	(211573391, "Finanças", "Contabilista");
 
 
 INSERT INTO empregado_de_obra(nif, anos_experiencia, cargo, especialidade)
 VALUES
-	(211000091, 7,  "Chefe de Obra", "Eletricista"),
+	(214572391, 7,  "Chefe de Obra", "Eletricista"),
 	(243095478, 2,  "Empregado",     "Picheleiro"),
 	(290345724, 1,  "Empregado",     "Pedreiro"),
 	(134590229, 13, "Chefe de Obra", "Pintor"),
@@ -126,10 +125,10 @@ VALUES
 
 INSERT INTO cria(nif, nr_projeto)
 VALUES
-	(211573391, 1),
-	(211573391, 2),
-	(211573391, 3),
-	(211573391, 4);
+	(211003392, 1),
+	(293373391, 1),
+	(211003392, 3),
+	(211003392, 4);
 
 
 INSERT INTO material_alocado(codigo_material, nr_obra)
@@ -185,12 +184,12 @@ VALUES
 	(119355101, "Camião"),
 	(210098723, "Comercial"),
 	(210098723, "Camião"),
-	(210098723, "Retroescavadora"),
+	(210098723, "Retroescavadora");
 	
 
-INSERT INTO cliente_obra_projeto(cliente, obra, projeto)
+INSERT INTO cliente_obra_projeto(nif_cliente, nr_obra, nr_projeto)
 VALUES
-	(239143555, 1, 1)
-	(223459225, 2, 2)
-	(133657483, 3, 3)
-	(248564348, 4, 4)
+	(239143555, 1, 1),
+	(223459225, 2, 2),
+	(133657483, 3, 3),
+	(248564348, 4, 4);
