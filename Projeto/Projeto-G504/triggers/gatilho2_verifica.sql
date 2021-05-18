e45c46691fe1d3a0209c093dc97453375cac8479
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-05-18 17:53:59 +0100

    Updates second trigger for ease of readability

--- Projeto/Projeto-G504/triggers/gatilho2_verifica.sql
+++ Projeto/Projeto-G504/triggers/gatilho2_verifica.sql
@@ -3,13 +3,13 @@
 .nullvalue NULL
 
 SELECT * FROM cliente WHERE nif = 239143555;
-SELECT * FROM projeto;
-SELECT * FROM cria;
-SELECT nr_obra, estado FROM obra;
+SELECT * FROM projeto WHERE nr_projeto = 1;
+SELECT * FROM cria WHERE nr_projeto = 1;
+SELECT nr_obra, estado FROM obra WHERE nr_obra = 1;
 
 DELETE FROM cliente_obra_projeto WHERE nif_cliente = 239143555;
 
-SELECT * FROM cliente;
-SELECT * FROM projeto;
-SELECT * FROM cria;
-SELECT nr_obra, estado FROM obra;
+SELECT * FROM cliente WHERE nif = 239143555;
+SELECT * FROM projeto WHERE nr_projeto = 1;
+SELECT * FROM cria WHERE nr_projeto = 1;
+SELECT nr_obra, estado FROM obra WHERE nr_obra = 1;
