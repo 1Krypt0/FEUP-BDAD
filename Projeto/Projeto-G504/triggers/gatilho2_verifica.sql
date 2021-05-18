3f67187a15675177c3e0524c340609fed5544d8f
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-05-18 22:23:37 +0100

    Changes queries to be easier to understand

--- Projeto/Projeto-G504/triggers/gatilho2_verifica.sql
+++ Projeto/Projeto-G504/triggers/gatilho2_verifica.sql
@@ -4,12 +4,10 @@
 
 SELECT * FROM cliente WHERE nif = 239143555;
 SELECT * FROM projeto WHERE nr_projeto = 1;
-SELECT * FROM cria WHERE nr_projeto = 1;
 SELECT nr_obra, estado FROM obra WHERE nr_obra = 1;
 
 DELETE FROM cliente_obra_projeto WHERE nif_cliente = 239143555;
 
 SELECT * FROM cliente WHERE nif = 239143555;
 SELECT * FROM projeto WHERE nr_projeto = 1;
-SELECT * FROM cria WHERE nr_projeto = 1;
 SELECT nr_obra, estado FROM obra WHERE nr_obra = 1;
--- Projeto/Projeto-G504/triggers/gatilho3_verifica.sql
+++ Projeto/Projeto-G504/triggers/gatilho3_verifica.sql
@@ -2,8 +2,9 @@
 .headers ON
 .nullvalue NULL
 
-SELECT * FROM localizacao;
+SELECT * FROM localizacao WHERE cod_postal LIKE '%4050-%';
 
 INSERT INTO obra VALUES (61, "2021-05-17", 120000, 200, 100, "Por aprovar", "Rua da Boavista", 886, "4050-106");
 
-SELECT * FROM localizacao;
+SELECT * FROM localizacao WHERE cod_postal LIKE '%4050-%';
+
